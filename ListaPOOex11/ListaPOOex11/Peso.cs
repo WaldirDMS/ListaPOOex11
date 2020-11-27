19e3d55930ad9f02ffd35379df0b0a80440d95f6
WaldirDMS <75144213+WaldirDMS@users.noreply.github.com>
2020-11-27 19:04:54 -0300

    Add files via upload

---
+++ ListaPOOex11/ListaPOOex11/Peso.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListaPOOex11
+{
+    class Peso
+    {
+        private double altura;
+        private double peso;
+        private double imc;
+
+        public void setPeso(double p)
+        {
+            peso = p;
+        }
+
+        public void setAltura(double p)
+        {
+            altura = p;
+        }
+
+        public void calcularImc()
+        {
+            imc = (altura / (peso * peso));
+
+            if (imc < 20)
+            {
+                Console.WriteLine("Abaixo do peso");
+            }
+
+            else
+            {
+                if (imc >= 25)
+                {
+                    Console.WriteLine("Acima do peso");
+                }
+
+                else
+                {
+                    Console.WriteLine("Peso ideal");
+                }
+            }
+        }
+    }
+}
---
+++ ListaPOOex11/ListaPOOex11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ListaPOOex11
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Peso peso;
+
+            peso = new Peso();
+
+            Console.Write("Informe o peso: ");
+            peso.setPeso(double.Parse(Console.ReadLine()));
+
+            Console.Write("Informe a altura: ");
+            peso.setAltura(double.Parse(Console.ReadLine()));
+
+            peso.calcularImc();
+        }
+    }
+}
+
